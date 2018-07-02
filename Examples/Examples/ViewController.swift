ca2f70cd4b42de4018163f305f64fe1a08566c4b
GEIVIINI <pai.pp61@gmail.com>
2018-07-02 17:55:52 +0700

    Init
    Pai - 0.8.0 - Commit setup tableview

--- Examples/Examples/ViewController.swift
+++ Examples/Examples/ViewController.swift
@@ -11,18 +11,15 @@ import MessageViewController
 
 class ViewController: MessageViewController, UITableViewDataSource, UITableViewDelegate, MessageAutocompleteControllerDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     var data = "Lorem ipsum dolor sit amet|consectetur adipiscing elit|sed do eiusmod|tempor incididunt|ut labore et dolore|magna aliqua| Ut enim ad minim|veniam, quis nostrud|exercitation ullamco|laboris nisi ut aliquip|ex ea commodo consequat|Duis aute|irure dolor in reprehenderit|in voluptate|velit esse cillum|dolore eu|fugiat nulla pariatur|Excepteur sint occaecat|cupidatat non proident|sunt in culpa|qui officia|deserunt|mollit anim id est laborum"
         .components(separatedBy: "|")
-    let users = ["rnystrom", "BasThomas", "jessesquires", "Sherlouk", "omwomw"]
+    let users = ["pai : ", "Deef : ", "Por : ", "Parn : ", "Unknown : "]
     var autocompleteUsers = [String]()
-    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        view.addSubview(tableView)
-
         borderColor = .lightGray
 
         messageView.textViewInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 16)
@@ -87,7 +84,8 @@ class ViewController: MessageViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if tableView === self.tableView {
-            cell.textLabel?.text = data[indexPath.row]
+            let number: Int = Int(arc4random_uniform(4))
+            cell.textLabel?.text = users[number] + data[indexPath.row]
         } else {
             cell.textLabel?.text = autocompleteUsers[indexPath.row]
         }
